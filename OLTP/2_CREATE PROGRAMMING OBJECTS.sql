6cc384283d9d374f584f478cf88f8b680762ca38
Maksim Gorbunov <maksim.v.gorbunov@yandex.ru>
2026-02-11 14:48:12 +0100

    code beautifulizing

--- OLTP/2_CREATE PROGRAMMING OBJECTS.sql
+++ OLTP/2_CREATE PROGRAMMING OBJECTS.sql
@@ -1,75 +1,83 @@
 --FIO-string generating function
-DROP FUNCTION IF EXISTS getString
-go
-CREATE FUNCTION getString (
+DROP FUNCTION IF EXISTS [dbo].[getString]
+GO
+CREATE FUNCTION	[dbo].[getString] 
+(
 	@p1 UNIQUEIDENTIFIER
 )
-RETURNS NVARCHAR(50)
+RETURNS NVARCHAR ( 50 )
 AS
 BEGIN
-	DECLARE @str NVARCHAR(255)
-	SELECT @str = CAST(@p1 as NVARCHAR(255))
+	DECLARE	@str NVARCHAR ( 255 )
 
-	SELECT @str = REPLACE(@str,NUMBER,'')
-	FROM (SELECT NUMBER FROM (
-							  VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9)
-							 ) as t(NUMBER)
-		 ) X
+	SELECT	@str = CAST ( @p1 AS NVARCHAR ( 255 ) )
 
-	RETURN REPLACE(@str,'-','')
+	SELECT	@str = REPLACE ( @str, [NUMBER], '' )
+	FROM (
+			SELECT	[NUMBER] 
+			FROM	(
+						VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9)
+					) AS t( [NUMBER] )
+		 ) AS [X]
+
+	RETURN REPLACE ( @str,'-','' )
 END
 
-go 
+GO 
 
 --card number generating function
-DROP FUNCTION IF EXISTS getCardNumber
-go
-CREATE FUNCTION getCardNumber (
-	@rnd FLOAT
+DROP FUNCTION IF EXISTS [dbo].[getCardNumber]
+GO
+CREATE FUNCTION		[dbo].[getCardNumber] 
+(
+	@p_rnd FLOAT
 )
 RETURNS NVARCHAR(16)
 AS
 BEGIN
 	DECLARE @res NVARCHAR(16)
-	SET @res = (SELECT RIGHT(STR(@rnd,25,8),8)+RIGHT(STR(@rnd,25,8),8))
 
-	RETURN @res
+	SET		@res = ( SELECT   RIGHT( STR( @p_rnd, 25, 8 ), 8 )
+							+ RIGHT( STR( @p_rnd, 25, 8 ), 8 )
+				   )
+	RETURN	@res
 END
 
 go
 
 --card duration generating function 
-DROP FUNCTION IF EXISTS getCardDuration
-go
-CREATE FUNCTION getCardDuration (
-	@rnd FLOAT
+DROP FUNCTION IF EXISTS [dbo].[getCardDuration]
+GO
+CREATE FUNCTION [dbo].[getCardDuration] 
+(
+	@p_rnd FLOAT
 )
 RETURNS DATE
 AS
 BEGIN
 	
-	DECLARE @res DATE,
-			@days INT
+	DECLARE @res	DATE
+		,	@days	INT
 
-	-- cards expire period will be from 1 till 9 days
-	SET @days = ROUND(@rnd*10,0)
-	SET @days = (SELECT CASE WHEN @days <> 0 THEN @days  
-						     ELSE 1 -- чтобы на 0 дней не выпустилась карта
+	-- cards expire period will be from 1 till 9 days from now
+	SET @days = ROUND ( @p_rnd*10, 0 )
+
+	SET @days = (	SELECT	CASE WHEN @days <> 0 THEN @days  
+								 ELSE 1 -- to avoid issuing card with 0 days
 					 END
 				) 
-	SET @res = (SELECT DATEADD(day,@days,GETDATE()))
+	SET @res =	(	SELECT DATEADD ( day, @days, GETDATE() ) )
 	
 	RETURN @res
-
 END
 
-go
+GO
 
 -- customer list generating procedure 
-DROP PROC IF EXISTS CREATE_CLIENTS_SP 
-go
-CREATE PROC CREATE_CLIENTS_SP 
-	@cntClients INT
+DROP PROCEDURE IF EXISTS [dbo].[CREATE_CLIENTS_SP] 
+GO
+CREATE PROCEDURE [dbo].[CREATE_CLIENTS_SP] 
+	@p_cntClients INT
 AS
 BEGIN
 	
@@ -77,78 +85,85 @@ BEGIN
 
 	DECLARE @cnt INT = 0
 
-	WHILE 1=1 BEGIN
-		IF @cnt = @cntClients
+	WHILE 1=1 
+	BEGIN
+		IF @cnt = @p_cntClients
 		BREAK
 
-		INSERT INTO Clients (Last_Name, First_Name, Middle_Name)
-		SELECT	dbo.getString(NEWID()),
-				dbo.getString(NEWID()),
-				dbo.getString(NEWID())
+		INSERT INTO [dbo].[Clients] 
+		(
+			[Last_Name]
+		,	[First_Name]
+		,	[Middle_Name]
+		)
+		SELECT	[dbo].[getString] ( NEWID() )
+			,	[dbo].[getString] ( NEWID() )
+			,	[dbo].[getString] ( NEWID() )
 	
 		SET @cnt +=1 
 	END
-
 END
 
-go 
+GO 
 
 -- transaction type generating function in range [1-4]
-DROP FUNCTION IF EXISTS getActionType
+DROP FUNCTION IF EXISTS [dbo].[getActionType]
 go
-CREATE FUNCTION getActionType (
-	@rnd FLOAT
+CREATE FUNCTION [dbo].[getActionType]
+(
+	@p_rnd FLOAT
 )
 RETURNS INT
 AS
 BEGIN
-	DECLARE @smallest	INT = 1,
-			@biggest	INT = 4,
-			@res		INT
+	DECLARE @smallest	INT = 1
+		,	@biggest	INT = 4
+		,	@res		INT
 
-	SET @res = FLOOR(@rnd*(@biggest-@smallest+1))+@smallest
+	SET @res = FLOOR( @p_rnd * ( @biggest-@smallest+1 )) + @smallest
 
 	RETURN @res
 END
 
-go
+GO
 
 --transaction amount generation function in range [1-10]
-DROP FUNCTION IF EXISTS getTransactionAmount
-go
-CREATE FUNCTION getTransactionAmount (
-	@rnd FLOAT
+DROP FUNCTION IF EXISTS [dbo].[getTransactionAmount]
+GO
+CREATE FUNCTION [dbo].[getTransactionAmount] 
+(
+	@p_rnd FLOAT
 )
-RETURNS NUMERIC(18,2)
+RETURNS NUMERIC ( 18, 2 )
 AS
 BEGIN
-	DECLARE @smallest	INT = 1,
-			@biggest	INT = 10,
-			@res		NUMERIC(18,2)
+	DECLARE @smallest	INT		= 1
+		,	@biggest	INT		= 10
+		,	@res		NUMERIC ( 18, 2 )
 
-	SET @res = ROUND(@rnd*(@biggest-@smallest+1)+@smallest,2)
+	SET @res = ROUND ( @p_rnd * ( @biggest-@smallest+1 ) + @smallest, 2 )
 
 	RETURN @res
 END
 
-go
+GO
 
 -- cards generation procedure
-DROP PROC IF EXISTS CREATE_CARDS_SP
-go
-CREATE PROC CREATE_CARDS_SP
-	@cntCards INT
+DROP PROC IF EXISTS [dbo].[CREATE_CARDS_SP]
+GO
+CREATE PROC [dbo].[CREATE_CARDS_SP]
+	@p_cntCards INT
 AS
 BEGIN
 	-- there is no unambiguous definition of cards quantity (by client or random by all ), 
-	-- so we'll generate @cntCards cards for every client 
+	-- so we'll generate @p_cntCards cards for every client 
 	SET NOCOUNT ON
 	
-	DECLARE @Client_ID	INT,
-			@cnt		INT
+	DECLARE @Client_ID	INT
+		,	@cnt		INT
 
 	DECLARE crClients CURSOR FAST_FORWARD READ_ONLY FOR
-	SELECT Client_ID FROM Clients
+	SELECT [Client_ID] FROM [dbo].[Clients]
 
 	OPEN crClients
 
@@ -158,20 +173,21 @@ BEGIN
 	BEGIN
 		SET @cnt = 0		
 		WHILE 1=1 BEGIN
-			IF @cnt = @cntCards
+			IF @cnt = @p_cntCards
 			BREAK
 			
-			INSERT INTO Cards (
-				Card_Number, 
-				Card_Holder, 
-				Valid_From, 
-				Valid_To
+			INSERT INTO [dbo].[Cards] 
+			(
+				Card_Number
+			,	Card_Holder
+			,	Valid_From
+			,	Valid_To
 			)
 			SELECT 
-				dbo.getCardNumber(RAND()), 
-				@Client_ID, 
-				GETDATE(), -- card date start - today 
-				dbo.getCardDuration(RAND()) -- card duration value is different for every card
+				[dbo].[getCardNumber]   ( RAND() )
+			,	@Client_ID
+			,	GETDATE ()							-- card date issuing - today 
+			,	[dbo].[getCardDuration] ( RAND() )	-- card duration value is different for every card
 
 			SET @cnt +=1
 		END
@@ -184,29 +200,34 @@ BEGIN
 
 END
 
-go
+GO
 
 -- inserting transactions procedure
-DROP PROC IF EXISTS INSERT_TRANSACTIONS_SP
-go
-CREATE PROC INSERT_TRANSACTIONS_SP
-	@cntTrans	INT,
-	@Trans_dt	DATE
+DROP PROCEDURE IF EXISTS [dbo].[INSERT_TRANSACTIONS_SP]
+GO
+CREATE PROCEDURE [dbo].[INSERT_TRANSACTIONS_SP]
+	@p_cntTrans	INT
+,	@p_Trans_dt	DATE
 AS 
 BEGIN
 	-- there is no unambiguous definition of transactions quantity (by client, by day or random by all ), 
-	-- so we'll generate @cntTrans transactions for every card 
+	-- so we'll generate @p_cntTrans transactions for every card 
 	SET NOCOUNT ON
 
-	DECLARE @Card_ID	INT,
-			@Valid_From	DATE,
-			@Valid_To	DATE,
-			@TransactionAmount	INT,
-			@Action_Type SMALLINT,
-			@cnt		INT
+	DECLARE @Card_ID			INT
+		,	@Valid_From			DATE
+		,	@Valid_To			DATE
+		,	@TransactionAmount	INT
+		,	@Action_Type		SMALLINT
+		,	@cnt				INT
 
 	DECLARE crCards CURSOR FAST_FORWARD READ_ONLY FOR
-	SELECT Card_ID, Valid_From, Valid_To FROM Cards
+	SELECT 
+		[Card_ID]
+	,	[Valid_From]
+	,	[Valid_To] 
+
+	FROM [dbo].[Cards]
 
 	OPEN crCards
 
@@ -216,51 +237,64 @@ BEGIN
 	BEGIN
 		SET @cnt = 0		
 		WHILE 1=1 BEGIN
-			IF @cnt = @cntTrans
+			IF @cnt = @p_cntTrans
 			BREAK
 			
-			SET @Action_Type = dbo.getActionType(RAND())
-			SET @TransactionAmount = dbo.getTransactionAmount(RAND())
+			SET @Action_Type = [dbo].[getActionType]( RAND() )
+			SET @TransactionAmount = [dbo].[getTransactionAmount]( RAND() )
 
-			IF @Trans_dt NOT BETWEEN @Valid_From AND @Valid_To BEGIN
-				INSERT INTO LogMessages ([text])
-				SELECT N'Card '+Card_Number+N' is expired'
-				FROM	Cards
-				WHERE	Card_ID = @Card_ID
+			IF @p_Trans_dt NOT BETWEEN @Valid_From AND @Valid_To 
+			BEGIN
+				INSERT INTO [dbo].[LogMessages] ( [text] )
+				SELECT N'Card ' + [Card_Number] + N' is expired'
+				FROM	[dbo].[Cards]
+				WHERE	[Card_ID] = @Card_ID
 
 				BREAK
 			END
 			ELSE 
-				IF  (SELECT [sign] FROM Action_Types WHERE Action_Type_ID = @Action_Type) = -1
+				IF  (	SELECT	[sign]
+						FROM	[dbo].[Action_Types]
+						WHERE	[Action_Type_ID] = @Action_Type
+					) = -1
 					AND 
-					(SELECT Balance FROM Cards WHERE Card_ID = @Card_ID) < @TransactionAmount BEGIN
-						INSERT INTO LogMessages([text])
-						SELECT N'Card remain '+Card_Number+N' is not enough for transactioning'+
-								(SELECT Action_Type_Desc FROM Action_Types WHERE Action_Type_Id = @Action_Type)+
-								N' amount '+ CAST(@TransactionAmount as NVARCHAR(20))+
-								N'. Avaliable funds: '+CAST(Balance as NVARCHAR(20)) 
-						FROM	Cards
-						WHERE	Card_ID = @Card_ID
+					(	SELECT	[Balance] 
+						FROM	[dbo].[Cards]
+						WHERE	[Card_ID] = @Card_ID
+					) < @TransactionAmount 
+				BEGIN
+						INSERT INTO [dbo].[LogMessages] ( [text] )
+						SELECT	N'Card remain ' + Card_Number + N' is not enough for transactioning'
+								+ (	SELECT	[Action_Type_Desc] 
+									FROM	[dbo].[Action_Types] 
+									WHERE	[Action_Type_Id] = @Action_Type
+								  )
+								+ N' amount '+ CAST(@TransactionAmount AS NVARCHAR( 20 ))
+								+ N'. Avaliable funds: '+ CAST( [Balance] AS NVARCHAR( 20 )) 
+						FROM	[dbo].[Cards]
+						WHERE	[Card_ID] = @Card_ID
 					
 					BREAK
-					END
+				END
 				ELSE
 					BEGIN TRAN
 
-						INSERT INTO Transactions					
-						SELECT	NEWID(),
-								@Card_ID,
-								@Trans_dt,
-								@Action_Type,
-								@TransactionAmount
+						INSERT INTO [dbo].[Transactions]
+						SELECT	NEWID()
+							,	@Card_ID
+							,	@p_Trans_dt
+							,	@Action_Type
+							,	@TransactionAmount
 
-						UPDATE	c
-						SET		Balance = Balance + @TransactionAmount*CA.[sign]
-						FROM Cards c
+						UPDATE	[c]
+						SET		[Balance] = [Balance] + @TransactionAmount * [CA].[sign]
+						FROM	[dbo].[Cards] AS [c]
 							CROSS APPLY (
-								SELECT [sign] FROM Action_Types WHERE Action_Type_ID = @Action_Type
-							) CA
-						WHERE c.Card_ID = @Card_ID
+								SELECT	[sign] 
+								FROM	[dbo].[Action_Types]
+								WHERE	[Action_Type_ID] = @Action_Type
+							) AS [CA]
+						WHERE	[c].[Card_ID] = @Card_ID
 
 					COMMIT TRAN
 			SET @cnt +=1
@@ -273,4 +307,3 @@ BEGIN
 	DEALLOCATE crCards
 		
 END
-
--- OLTP/3_GENERATE_DATA_SP.sql
+++ OLTP/3_GENERATE_DATA_SP.sql
@@ -1,27 +1,29 @@
 /*
 	EXEC GENERATE_DATA_SP 10,1,1,'20210714'
 */
-DROP PROC IF EXISTS GENERATE_DATA_SP
-go
-CREATE PROC GENERATE_DATA_SP (
-	@cntClients	INT,
-	@cntCards	INT,
-	@cntTrans	INT,
-	@Trans_dt	DATE
+DROP PROC IF EXISTS [dbo].[GENERATE_DATA_SP]
+GO
+CREATE PROC [dbo].[GENERATE_DATA_SP] 
+(
+	@p_cntClients	INT
+,	@p_cntCards		INT
+,	@p_cntTrans		INT
+,	@p_Trans_dt		DATE
 )
 AS
 BEGIN
 	
 	SET NOCOUNT ON 
 		
-	-- generating @cntClients clients
-	EXEC CREATE_CLIENTS_SP @cntClients
+	-- generating @p_cntClients clients
+	EXEC [dbo].[CREATE_CLIENTS_SP]			@p_cntClients
 	
-	-- creating @cntCards for every client
-	EXEC CREATE_CARDS_SP @cntCards
+	-- creating @p_cntCards for every client
+	EXEC [dbo].[CREATE_CARDS_SP]			@p_cntCards
 
 	-- generating @cntTrans transactions
-	EXEC INSERT_TRANSACTIONS_SP @cntTrans, @Trans_dt
+	EXEC [dbo].[INSERT_TRANSACTIONS_SP]		@p_cntTrans
+										,	@p_Trans_dt
 
 END
 
